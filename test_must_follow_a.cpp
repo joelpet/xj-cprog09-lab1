4366c459fe34318c291e2f9c8262023804c54a3e
ecksun <ecksun@gmail.com>
2010-01-08 20:49:12 +0100

    Hämtade hem testet vi gjorde under redovisningen

--- test_must_follow_a.cpp
+++ test_must_follow_a.cpp
@@ -10,13 +10,24 @@ public:
         char vek[] = {'x', 'x', 'a', 'b', 'x'};
         int result = must_follow_a(vek, 4, 'a', 'b');
         TS_ASSERT_EQUALS( result, 1);
+    }
+    void test_a_is_third_to_last(void) {
 
         char vek2[] = {'b', 'b', 'b', 'a', 'b', 'b'};
-        result = must_follow_a(vek2, 3, 'a', 'b');
-        TS_ASSERT_EQUALS(result, 2);
+        int result = must_follow_a(vek2, 3, 'a', 'b');
+        TS_ASSERT_EQUALS(result, 1);
+    }
+
+    void test_a_is_last(void) {
 
         char vek3[] = {'b', 'b', 'b', 'a'};
-        result = must_follow_a(vek3, 3, 'a', 'b');
+        int result = must_follow_a(vek3, 3, 'a', 'b');
         TS_ASSERT_EQUALS(result, 0);
+    }
+    void test_a_is_first(void) {
+
+        char vek3[] = {'b', 'b', 'b', 'a'};
+        int result = must_follow_a(vek3, 0, 'b', 'b');
+        TS_ASSERT_EQUALS(result, 2);
     }
 };
