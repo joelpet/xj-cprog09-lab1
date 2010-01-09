afeef3a2ad84f60c3a79c168cd498d6c8267958e
Joel Pettersson <petterssonjoel@gmail.com>
2010-01-09 22:54:03 +0100

    Added some matrix tests and updated Makefile.

--- Matrix/tests_runner.cpp
+++ Matrix/tests_runner.cpp
@@ -21,16 +21,28 @@ static MatrixTestSuite suite_MatrixTestSuite;
 static CxxTest::List Tests_MatrixTestSuite = { 0, 0 };
 CxxTest::StaticSuiteDescription suiteDescription_MatrixTestSuite( "matrix_test_suite.h", 5, "MatrixTestSuite", suite_MatrixTestSuite, Tests_MatrixTestSuite );
 
-static class TestDescription_MatrixTestSuite_test_test : public CxxTest::RealTestDescription {
+static class TestDescription_MatrixTestSuite_test_direct_mutate_access : public CxxTest::RealTestDescription {
 public:
- TestDescription_MatrixTestSuite_test_test() : CxxTest::RealTestDescription( Tests_MatrixTestSuite, suiteDescription_MatrixTestSuite, 28, "test_test" ) {}
- void runTest() { suite_MatrixTestSuite.test_test(); }
-} testDescription_MatrixTestSuite_test_test;
+ TestDescription_MatrixTestSuite_test_direct_mutate_access() : CxxTest::RealTestDescription( Tests_MatrixTestSuite, suiteDescription_MatrixTestSuite, 35, "test_direct_mutate_access" ) {}
+ void runTest() { suite_MatrixTestSuite.test_direct_mutate_access(); }
+} testDescription_MatrixTestSuite_test_direct_mutate_access;
 
-static class TestDescription_MatrixTestSuite_test_direct_access : public CxxTest::RealTestDescription {
+static class TestDescription_MatrixTestSuite_test_direct_const_access : public CxxTest::RealTestDescription {
 public:
- TestDescription_MatrixTestSuite_test_direct_access() : CxxTest::RealTestDescription( Tests_MatrixTestSuite, suiteDescription_MatrixTestSuite, 32, "test_direct_access" ) {}
- void runTest() { suite_MatrixTestSuite.test_direct_access(); }
-} testDescription_MatrixTestSuite_test_direct_access;
+ TestDescription_MatrixTestSuite_test_direct_const_access() : CxxTest::RealTestDescription( Tests_MatrixTestSuite, suiteDescription_MatrixTestSuite, 42, "test_direct_const_access" ) {}
+ void runTest() { suite_MatrixTestSuite.test_direct_const_access(); }
+} testDescription_MatrixTestSuite_test_direct_const_access;
+
+static class TestDescription_MatrixTestSuite_test_cout_printing : public CxxTest::RealTestDescription {
+public:
+ TestDescription_MatrixTestSuite_test_cout_printing() : CxxTest::RealTestDescription( Tests_MatrixTestSuite, suiteDescription_MatrixTestSuite, 48, "test_cout_printing" ) {}
+ void runTest() { suite_MatrixTestSuite.test_cout_printing(); }
+} testDescription_MatrixTestSuite_test_cout_printing;
+
+static class TestDescription_MatrixTestSuite_test_cin_reading : public CxxTest::RealTestDescription {
+public:
+ TestDescription_MatrixTestSuite_test_cin_reading() : CxxTest::RealTestDescription( Tests_MatrixTestSuite, suiteDescription_MatrixTestSuite, 54, "test_cin_reading" ) {}
+ void runTest() { suite_MatrixTestSuite.test_cin_reading(); }
+} testDescription_MatrixTestSuite_test_cin_reading;
 
 #include <cxxtest/Root.cpp>
